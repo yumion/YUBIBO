75195b52a5972f15c0fd6f00440b61ff7ca44b0d
Murata <4318571@ed.tus.ac.jp>
2019-03-16 15:28:00 +0900

    add enter space delete

--- finger_keyboard.py
+++ finger_keyboard.py
@@ -4,75 +4,96 @@ from time import sleep
 import os
 import pyautogui as pgui  #pythonからキーボードを操作
 
-## Arduinoに信号を送信
-'''Arduino SDKを起動し，シリアルモニタは開かない'''
-import serial
-ser = serial.Serial()
-ser.baudrate = 115200
+# pgui.KEYBOARD_KEYS
+
+# Arduinoに信号を送信
+# Arduino SDKを起動し，シリアルモニタは開かない
+# import serial
+# ser = serial.Serial()
+# ser.baudrate = 115200
+
+## MACを使う場合
 # for file in os.listdir('/dev'):
 #     if "cu.usbserial-" in file:
-ser.port = 'COM3'
-ser.open() # シリアルモニタを開く
-sleep(2) # 開いてから信号を送信するまで2秒待つ必要がある
+
+# winを使う場合
+# ser.port = 'COM3'
+# ser.open() # シリアルモニタを開く
 
 ######### 入力 #########
-# UP:0 DOWN:1
-# i:2 a:3 ka:4 sa:5 se:6
-# f2:7 f3:8 f4:9 f5:10
-# 何もなし:11
+# あ:0　か:1　さ:2
+# た:3　な:4　は:5
+# ま:6　や:7　ら:8
+# 濁点、半濁点、小文字:9　わ:10　:11
+# space:12 delete:13 enter:14
+# 何もなし:15
 ########################
-
-consonant = 0 # 子音
 count = 0
-consonant_words = ['', 'k', 's']
-# consonant_words = [['', 'k', 's'],['t','n','h'],['m','y','r'],['','y','r']]
-# consonant_words = [['あ', 'い', 'う', 'え', 'お'],
-#                 ['か', 'き', 'く', 'け', 'こ'],
-#                 ['さ', 'し', 'す', 'せ', 'そ'],
-#                 ['た', 'ち', 'つ', 'て', 'と'],
-#                 ['な', 'に', 'ぬ', 'ね', 'の'],
-#                 ['は', 'ま', 'や', 'ら', 'わ'],
-#                 ['ま', 'み', 'む', 'め', 'も'],
-#                 ['や', '', 'ゆ', '', 'よ'],
-#                 ['ら', 'り', 'る', 'れ', 'ろ'],
-#                 ['わ', 'を', 'ん', '', '']]
-
+op_count = 0
+pre_location = None
+consonant_words = ['', 'k', 's', 't', 'n', 'h', 'm', 'y','r','','w','']
+voiced_consonant_words = ['', 'g', 'z', 'd', '', 'b']
+vowel_words = ['a','i','u','e','o']
 
 print('start')
-while True:
-    read = ser.readline()
-    location = int(read.strip().decode('utf-8'))
-     # = int(read.strip().decode('utf-8')) # stripで余分な文字列を排除
-    print(count, location)
-    if consonant == 0:
-        if location in np.arange(3,6): # 真ん中3つのボタンをタッチ
-            consonant = location # 子音を決定
-            consonant_word = consonant_words[consonant-3]
 
-    else:
-        pgui.press('kana')
-        pgui.press('kana') # Windowsでは2回必要
-        if location == consonant - 1:
-            pgui.typewrite(consonant_word+'i')
+pgui.press('kana')
+pgui.press('kana') # Windowsでは2回必要
+
+while True:
+    location = int(input())
+
+    # read = ser.readline()
+    # location = int(read.strip().decode('utf-8')) # stripで余分な文字列を排除
+
+    count = count % 5
+
+    print(count, op_count, location)
+
+    if location != 15 and location != 9 and location < 12: #入力なし、濁点、11意外入力された場合
+        if pre_location != location:
             count = 0
-            consonant = 0
-        elif location == 0:
-            pgui.typewrite(consonant_word+'u')
-            count = 0
-            consonant = 0
-        elif location == consonant + 1:
-            pgui.typewrite(consonant_word+'e')
-            count = 0
-            consonant = 0
-        elif location == 1:
-            pgui.typewrite(consonant_word+'o')
-            count = 0
-            consonant = 0
-        elif count == 10:
-            pgui.typewrite(consonant_word+'a')
-            count = 0
-            consonant = 0
-        sleep(1)
+            pgui.typewrite(consonant_words[location]+vowel_words[count])
+            # print(consonant_words[location]+vowel_words[count])
+            pre_location = location
+
+        else:
+            pgui.press('backspace')
+            pgui.typewrite(consonant_words[location]+vowel_words[count])
+            # print(consonant_words[location]+vowel_words[count])
+
         count += 1
 
-ser.close()
+    elif location == 9: #濁点
+        # print('option')
+        if op_count == 0 and pre_location in [1,2,3,5]:
+            pgui.press('backspace')
+            pgui.typewrite(voiced_consonant_words[pre_location]+vowel_words[count-1])
+            # print('濁点')
+            # print(voiced_consonant_words[pre_location]+vowel_words[count-1])
+            if pre_location == 5 or (pre_location==3 and count==3): #
+                op_count+=1
+
+        elif op_count == 1 and pre_location == 5:
+            pgui.press('backspace')
+            pgui.typewrite('p'+vowel_words[count-1])
+            # print('半濁点')
+            # print('p'+vowel_words[count-1])
+            op_count=0
+
+        elif (pre_location in [0,7]) or (pre_location==3 and count==3):
+            pgui.press('backspace')
+            pgui.typewrite('x'+consonant_words[pre_location]+vowel_words[count-1])
+            op_count=0
+            # print('小文字')
+            # print('x'+consonant_words[pre_location]+vowel_words[count-1])
+
+    elif location ==12:
+        # pgui.press('space')
+        print('space')
+    elif location ==13:
+        # pgui.press('delete')
+        print('delete')
+    elif location ==14:
+        # pgui.press('enter')
+        print('enter')
